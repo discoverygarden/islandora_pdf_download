320a32aa223204b29c62095414615f3ca2e24e3c
MorganDawe <mdawe@discoverygarden.ca>
2016-04-29 16:59:57 -0300

    Updated code review feedback.

--- theme/islandora-pdf-download.tpl.php
+++ theme/islandora-pdf-download.tpl.php
@@ -1,13 +1,11 @@
 <?php
-
 /**
  * @file
- * Template for generating the bookmark list column view.
+ * Template for creating the download PDF Datastream link.
  *
  * Available variables:
- * - $list_links: Links to the other list views.
- * - $column_count: How many columns to print.
- * - $objects: An array keyed on PIDs contianing an array of markup pieces.
+ * - $message: The manual link to the download page
+ * - $islandora_object: The islandora object in question.
  */
 ?>
 
